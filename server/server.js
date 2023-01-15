7ea783f4db8daefeb5abd549957ac7284a6bf8d1
Deeraj <deerajrajesh22@gmail.com>
2023-01-15 10:42:59 +0530

    fixed bigs

--- server/server.js
+++ server/server.js
@@ -4,15 +4,19 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRoutes from "./routes/user.js";
 import blogRoutes from "./routes/blog.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://localhost:27017/MindTaleDB", {
+mongoose.connect(process.env.MONGODB_CONNECT_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -20,6 +24,8 @@ mongoose.connect("mongodb://localhost:27017/MindTaleDB", {
 app.use("/user", userRoutes);
 app.use("/blog", blogRoutes);
 
-app.listen(7000, () => {
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
   console.log("Server is working in 7000..");
 });
